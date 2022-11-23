ca4ad82b4ececd02e28fa53240e447a3db1b960a
tannirupravallika <tannirupravallika74@gmail.com>
2022-11-23 11:46:31 +0530

    SE first lab

---
+++ src/main/java/com/maven/Samplemavenexam/App.java
@@ -0,0 +1,13 @@
+package com.maven.Samplemavenexam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab interval1 Exam" );
+    }
+}
